c27213d1e1c47fac9328cf2db7c7090111e3486c
Serena Antonetti <45170586+serenastorm@users.noreply.github.com>
2021-12-07 11:47:33 +0000

    [implementing feedback]  remove copy email link on landing page

--- src/Main/components/LandingPageFooter/LandingPageFooter.tsx
+++ src/Main/components/LandingPageFooter/LandingPageFooter.tsx
@@ -1,17 +1,9 @@
-import { useCopyToClipboard } from "infrastructure/hooks";
 import { NewTabLink } from "Main/components";
 
 import "./LandingPageFooter.scss";
 
 const LandingPageFooter = () => {
   const contactEmail = "serena.antonetti@gmail.com";
-
-  const {
-    copyConfirmationProps,
-    copyInputProps,
-    onCopyButtonClick,
-    textCopiedToClipboard,
-  } = useCopyToClipboard(contactEmail);
 
   return (
     <footer>
@@ -22,32 +14,7 @@ const LandingPageFooter = () => {
           to={`${contactEmail}?subject=Hi`}
           type="email"
         />
-        <br />
-        <span className="smaller" {...copyConfirmationProps}>
-          {textCopiedToClipboard ? (
-            <>
-              Email address copied!
-              {/* Add fake button to prevent the height jumping when the copy changes */}
-              <button
-                className="email-link"
-                tabIndex={-1}
-                aria-hidden="true"
-                style={{ opacity: 0 }}
-              >
-                .
-              </button>
-            </>
-          ) : (
-            <>
-              If the email link doesn't work,{" "}
-              <button onClick={onCopyButtonClick} className="email-link">
-                click here to copy my email address
-              </button>
-            </>
-          )}
-        </span>
       </p>
-      <input {...copyInputProps} />
     </footer>
   );
 };
