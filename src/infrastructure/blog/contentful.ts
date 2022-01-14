95ce3f2ad39515daf9c21af9b711e6f9fcf25e92
Serena Antonetti <45170586+serenastorm@users.noreply.github.com>
2022-01-14 10:54:32 +0000

    [housekeeping] moving contentful IDs to constants file

---
+++ src/infrastructure/blog/contentful.ts
@@ -0,0 +1,43 @@
+import { BlogPostResponse } from "./types";
+import { accessToken, spaceId } from "./constants";
+
+export const client = require("contentful").createClient({
+  space: spaceId,
+  accessToken: accessToken.publishedContent,
+});
+
+const getPreviousPost = (currentPostDate: Date) =>
+  client
+    .getEntries({
+      order: "fields.date",
+      content_type: "blogPost",
+      limit: 1,
+      "fields.date[gt]": currentPostDate,
+    })
+    .then((response: { items: BlogPostResponse[] }) => response.items);
+
+const getNextPost = (currentPostDate: Date) =>
+  client
+    .getEntries({
+      order: "-fields.date",
+      content_type: "blogPost",
+      limit: 1,
+      "fields.date[lt]": currentPostDate,
+    })
+    .then((response: { items: BlogPostResponse[] }) => response.items);
+
+const getAllPosts = () =>
+  client
+    .getEntries({ order: "-fields.date", content_type: "blogPost" })
+    .then((response: { items: BlogPostResponse[] }) => response.items);
+
+const getSinglePost = (category: string, slug: string) =>
+  client
+    .getEntries({
+      "fields.slug": slug,
+      "fields.category": category,
+      content_type: "blogPost",
+    })
+    .then((response: { items: BlogPostResponse[] }) => response.items);
+
+export { getAllPosts, getSinglePost, getPreviousPost, getNextPost };
